96bf58acdee0c73d5693415709b30b2bab16d85b
kamm <kamm@ua.fm>
2015-10-24 22:16:53 +0300

    Add a map

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,30 @@
-$(document).ready(function(){
-  $('a[href^="#"], a[href^="."]').click( function(){ // если в href начинается с # или ., то ловим клик
+$(function () {
+  $('a[href^="#"], a[href^="."]').click(function () { // если в href начинается с # или ., то ловим клик
     var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href
     if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
-      $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500); // анимируем скроолинг к элементу scroll_el
+      $('html, body').animate({scrollTop: $(scroll_el).offset().top}, 500); // анимируем скроолинг к элементу scroll_el
     }
     return false; // выключаем стандартное действие
   });
+
+  $('#mapModal').on('shown.bs.modal', function initialize() {
+    var mapCanvas = document.getElementById('map');
+    var mapOptions = {
+      center: new google.maps.LatLng(49.514234, 31.288787),
+      zoom: 5,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      streetViewControl: false,
+      mapTypeControl: false,
+      zoomControlOptions: {}
+    };
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+
+    var marker = new google.maps.Marker({
+      map: map,
+      animation: google.maps.Animation.DROP,
+      position: {lat: 48.466730, lng: 35.043740},
+      title: 'Dnipropetrovsk',
+      icon: 'images/markerMap.png'
+    });
+  })
 });
